c4d84be35d046a765d2c47edcf9fce9ac2dc2b92
LinZeyo <13467579+w667985@user.noreply.gitee.com>
2025-02-27 10:31:26 +0800

    忽略idea文件夹，User添加构造方法

--- src/main/java/com/linze/pojo/User.java
+++ src/main/java/com/linze/pojo/User.java
@@ -4,31 +4,14 @@ public class User {
     int uid;
     String uname;
     int uage;
-//    public User() {
-//    }
-//    public User(int uid, String uname, String uage) {
-//        this.uid = uid;
-//        this.uname = uname;
-//        this.uage = uage;
-//    }
-    public int getUid() {
-        return uid;
+    public User() {
     }
-    public void setUid(int uid) {
+    public User(int uid, String uname, int uage) {
         this.uid = uid;
-    }
-    public String getUname() {
-        return uname;
-    }
-    public void setUname(String uname) {
         this.uname = uname;
-    }
-    public int getUage() {
-        return uage;
-    }
-    public void setUage(int uage) {
         this.uage = uage;
     }
+
     @Override
     public String toString() {
         return "User{" +
@@ -37,4 +20,30 @@ public class User {
                 ", uage=" + uage +
                 '}';
     }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public int getUage() {
+        return uage;
+    }
+
+    public void setUage(int uage) {
+        this.uage = uage;
+    }
+
+
 }
